159362d9d74a4863db99e22d0c4f38d32d850d37
Robert Collins <beaucollins@gmail.com>
2016-01-29 15:12:06 -0800

    Fixing chat subdomain

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ if ( environment === 'development' ) {
 	app.use( subdomain( 'chat', require( './dev' ).middleware ) )
 } else {
 	app.use( '/', withTeam( serveStatic( './dist/' ) ) )
-	app.use( '/', subdomain( 'chat', withTeam( serveStatic( './dist/' ) ) ) )
+	app.use( '/', subdomain( 'chat', serveStatic( './dist/' ) ) )
 }
 
 server.listen( process.env.PORT || 3000, () => {
